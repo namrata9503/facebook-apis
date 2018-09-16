7a7dffe9bb928bca98eb627833d9f9dd8db15663
namrata <nam9503@gmail.com>
2018-09-16 11:25:55 +0530

    added user model

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost:27017/facebook-apis')
+mongoose.connection.on('error', (error) => console.error(error))
+mongoose.connection.on('open', () => console.log('successfully connected with mongodb..'))
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
